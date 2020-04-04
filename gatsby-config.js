85c6279bd7209dadd9e24b646b8ec5b6547efaeb
Carlos Gomez <carliros.g@gmail.com>
2020-04-03 22:23:28 -0400

    Impro icon

--- gatsby-config.js
+++ gatsby-config.js
@@ -19,13 +19,13 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: `gatsby-starter-default`,
-        short_name: `starter`,
+        name: `Radio Nueva Vida`,
+        short_name: `rnvsp`,
         start_url: `/`,
         background_color: `#663399`,
         theme_color: `#663399`,
         display: `minimal-ui`,
-        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+        icon: `src/images/rnvsp-small-icon.png`,
       },
     },
     // this (optional) plugin enables Progressive Web App + Offline functionality
